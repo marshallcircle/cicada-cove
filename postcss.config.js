53ee607d6caaab8db05c5cbee60ed843343ede09
marshallcircle <nickmarshall845@gmail.com>
2025-03-29 12:55:05 -0400

    Enhance PostCSS config with optimizations for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false }
+    },
+    ...(process.env.NODE_ENV === 'production' ? {
+      'cssnano': {
+        preset: 'default',
+      }
+    } : {})
   },
 };
