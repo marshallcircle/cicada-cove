d8c94a75e2c08ccbb3e3818b8063bd56b8f6ae8e
marshallcircle <nickmarshall845@gmail.com>
2025-03-30 00:26:48 -0400

    Add currency formatter utility

---
+++ lib/utils/formatters.ts
@@ -0,0 +1,11 @@
+/**
+ * Format a number as US currency.
+ */
+export const formatCurrency = (amount: number): string => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount);
+};
